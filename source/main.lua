f95b87a54aad9a77d27c773c889fbc3b782ecdca
Akshay Bharwani <akshaybharwani@pm.me>
2023-07-23 21:00:14 +0900

    Draw basic weapon

--- source/main.lua
+++ source/main.lua
@@ -1,16 +1,35 @@
+import "CoreLibs/object"
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-local playerX, playerY = 200, 120
-local playerRadius = 10
-local playerSpeed = 3
+-- local playerX, playerY = 200, 120
+-- local playerRadius = 10
+-- local playerSpeed = 3
 
-function playdate.update()
-	gfx.clear()
-    local crankAngle = math.rad(pd.getCrankPosition())
-    playerX += math.sin(crankAngle) * playerSpeed
-	playerY -= math.cos(crankAngle) * playerSpeed
-	gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
+-- WeaponSize
+local weaponBaseSize = 20
+
+local maxScreenWidth = 400
+local maxScreenHeight = 240
+
+function pd.update()
+    gfx.clear()
+	-- Update stuff every frame
+    gfx.sprite.update()
+    pd.timer.updateTimers()
+
+    -- local crankAngle = math.rad(pd.getCrankPosition())
+    -- playerX += math.sin(crankAngle) * playerSpeed
+    -- playerY -= math.cos(crankAngle) * playerSpeed
+
+	-- Draw Weapon
+    gfx.fillRect((maxScreenWidth - weaponBaseSize) / 2,
+        maxScreenHeight - weaponBaseSize,
+        weaponBaseSize, weaponBaseSize)
+    gfx.drawLine((maxScreenWidth / 2),
+		maxScreenHeight - weaponBaseSize, 200, 120)
 end
