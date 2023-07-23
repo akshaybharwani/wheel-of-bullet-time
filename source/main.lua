447c7233764fda00d9f2b8306bc7aca6bf3aec27
Akshay Bharwani <akshaybharwani@pm.me>
2023-07-23 22:48:15 +0900

    ADD preliminary weapon head rotation

--- source/main.lua
+++ source/main.lua
@@ -5,6 +5,7 @@ import "CoreLibs/timer"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
+local geometry <const> = pd.geometry
 
 -- local playerX, playerY = 200, 120
 -- local playerRadius = 10
@@ -30,6 +31,41 @@ function pd.update()
     gfx.fillRect((maxScreenWidth - weaponBaseSize) / 2,
         maxScreenHeight - weaponBaseSize,
         weaponBaseSize, weaponBaseSize)
-    gfx.drawLine((maxScreenWidth / 2),
-		maxScreenHeight - weaponBaseSize, 200, 120)
+    -- Given points and angle
+    local pointX, pointY = 200, 180
+    local centerX, centerY = maxScreenWidth / 2, maxScreenHeight - weaponBaseSize
+    local crankPosition = pd.getCrankPosition()
+    if crankPosition > 85 and crankPosition < 270 then
+        crankPosition = 85
+    elseif crankPosition < 270 and crankPosition > 85 then
+        crankPosition = 270
+    end
+
+    local rotationAngle = math.rad(crankPosition)
+
+    if rotationAngle > 85 then
+        rotationAngle = 85
+    elseif rotationAngle < -85 then
+        rotationAngle = -85
+    end
+
+    -- Rotate the point around the center by the specified angle
+    local rotatedX, rotatedY = rotatePoint(pointX, pointY, centerX, centerY, rotationAngle)
+
+    -- Use the rotated coordinates to draw the line
+    gfx.drawLine(centerX, centerY, rotatedX, rotatedY)
+end
+
+-- Function to rotate a point (x, y) around another point (cx, cy) by a specified angle (in radians)
+function rotatePoint(x, y, cx, cy, angle)
+    local cosAngle = math.cos(angle)
+    local sinAngle = math.sin(angle)
+    local dx = x - cx
+    local dy = y - cy
+
+    -- Calculate the new coordinates after rotation
+    local rotatedX = cx + dx * cosAngle - dy * sinAngle
+    local rotatedY = cy + dx * sinAngle + dy * cosAngle
+
+    return rotatedX, rotatedY
 end
