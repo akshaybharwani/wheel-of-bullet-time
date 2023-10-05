30a2f4cacc551109f38ffd310c90c4416660a90a
Akshay Bharwani <akshaybharwani@pm.me>
2023-10-05 20:32:12 +0530

    ADD bullet and number sprites for bullet display

--- source/scripts/bulletDisplay.lua
+++ source/scripts/bulletDisplay.lua
@@ -3,28 +3,53 @@ local gfx <const> = pd.graphics
 
 class('BulletDisplay').extends(gfx.sprite)
 
+local bulletImagePath = "images/ui/UI_bullet_8x16"
+local numbersImagePath = "images/ui/UI_numbers-table-8-16"
+
+local totalBulletDisplayWidth = 34
+local numberPadding = 1
+
 function BulletDisplay:init()
-    self:setCenter(0, 0)
-    -- TODO: very magic numbery. change.
-    self.displayString = string.format("%03d", CURRENT_BULLET_COUNT)
-    local displayTextWidth, displayTextHeight = gfx.getTextSize(self.displayString)
-    self:moveTo(MAX_SCREEN_WIDTH / 2 - displayTextWidth / 2, 223)
-    -- Important, so you don't have it moving in
-    -- the world space
-    --self:setIgnoresDrawOffset(true)
-    self:setZIndex(UI_Z_INDEX)
+    self.numbersImageTable = gfx.imagetable.new(numbersImagePath)
+
+    self.bulletSprite = gfx.sprite.new(gfx.image.new(bulletImagePath))
+
+    self.bulletSpriteY = MAX_SCREEN_HEIGHT - self.bulletSprite.height / 2
+    self.bulletSpriteX = MAX_SCREEN_WIDTH / 2 + self.bulletSprite.width / 2 - (totalBulletDisplayWidth / 2)
+
+    self.bulletSprite:moveTo(self.bulletSpriteX, self.bulletSpriteY)
+    self.bulletSprite:setZIndex(UI_Z_INDEX)
+    self.bulletSprite:add()
+
+    self.bulletCountString = string.format("%03d", CURRENT_BULLET_COUNT)
+
+    local firstNumber = string.sub(self.bulletCountString, 1, 1)
+    self.firstNumber = self:getNumberSprite(firstNumber, self.bulletSpriteX + self.bulletSprite.width)
+
+    local secondNumber = string.sub(self.bulletCountString, 2, 2)
+    self.secondNumber = self:getNumberSprite(secondNumber, self.bulletSpriteX + self.bulletSprite.width * 2 + numberPadding)
+
+    local thirdNumber = string.sub(self.bulletCountString, 3, 3)
+    self.thirdNumber = self:getNumberSprite(thirdNumber, self.bulletSpriteX + self.bulletSprite.width * 3 + numberPadding * 2)
     self:add()
 end
 
 function BulletDisplay:update()
-    self.displayString = string.format("%03d", CURRENT_BULLET_COUNT)
-    -- Getting the width and height of the text to make sure the image
-    -- will always fit the text
-    local displayTextWidth, displayTextHeight = gfx.getTextSize(self.displayString)
-    local displayImage = gfx.image.new(displayTextWidth, displayTextHeight)
-    gfx.pushContext(displayImage)
-        gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
-        gfx.drawText(self.displayString, 0, 0)
-    gfx.popContext()
-    self:setImage(displayImage)
+    self.bulletCountString = string.format("%03d", CURRENT_BULLET_COUNT)
+    local firstNumber = string.sub(self.bulletCountString, 1, 1)
+    self.firstNumber:setImage(self.numbersImageTable:getImage(firstNumber + 1))
+
+    local secondNumber = string.sub(self.bulletCountString, 2, 2)
+    self.secondNumber:setImage(self.numbersImageTable:getImage(secondNumber + 1))
+
+    local thirdNumber = string.sub(self.bulletCountString, 3, 3)
+    self.thirdNumber:setImage(self.numbersImageTable:getImage(thirdNumber + 1))
+end
+
+function BulletDisplay:getNumberSprite(number, positionX)
+    local numberSprite = gfx.sprite.new(self.numbersImageTable:getImage(number + 1))
+    numberSprite:moveTo(positionX, self.bulletSpriteY)
+    numberSprite:setZIndex(UI_Z_INDEX)
+    numberSprite:add()
+    return numberSprite
 end
